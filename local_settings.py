06809b8d1e857344f48661231459dd4fe5b7290e
Ben Mountain <bemo1787@colorado.edu>
2021-03-04 00:55:51 -0700

    Changed python version from 3.6.2 to 3.6.13 for heroku compatability in runtime.txt. Updated tweet source accounts, URLs to scrape, host twitter account, tags and attributes to search for in URL and frequency of tweets in local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -15,27 +15,27 @@ MY_ACCESS_TOKEN_SECRET = environ.get('TWITTER_ACCESS_SECRET')#Your Access Token 
 # Configuration for Mastodon API
 ENABLE_MASTODON_SOURCES = False # Fetch mastodon statuses as a source?
 ENABLE_MASTODON_POSTING = False # Toot resulting status?
-MASTODON_API_BASE_URL = "" # an instance url like https://botsin.space
+MASTODON_API_BASE_URL = '' # an instance url like https://botsin.space
 CLIENT_CRED_FILENAME = '' # the MASTODON client secret file you created for this project
 USER_ACCESS_FILENAME = '' # The MASTODON user credential file you created at installation.
 
 # Sources (Twitter, Mastodon, local text file or a web page)
-TWITTER_SOURCE_ACCOUNTS = [""]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
-MASTODON_SOURCE_ACCOUNTS = [""] # A list, e.g. ["@user@instance.tld"]
+TWITTER_SOURCE_ACCOUNTS = ['meekmill', 'aoc']  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+MASTODON_SOURCE_ACCOUNTS = [''] # A list, e.g. ["@user@instance.tld"]
 SOURCE_EXCLUDE = r'^$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
 STATIC_TEST = False  # Set this to True if you want to test Markov generation from a static file instead of the API.
-TEST_SOURCE = ".txt"  # The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
-SCRAPE_URL = False  # Set this to true to scrape a webpage.
-SRC_URL = ['http://www.example.com/one', 'https://www.example.com/two']  # A comma-separated list of URLs to scrape
-WEB_CONTEXT = ['span', 'h2']  # A comma-separated list of the tag or object to search for in each page above.
-WEB_ATTRIBUTES = [{'class': 'example-text'}, {}] # A list of dictionaries containing the attributes for each page.
+TEST_SOURCE = '.txt'  # The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API. You can use the included testcorpus.txt, if needed.
+SCRAPE_URL = True  # Set this to true to scrape a webpage.
+SRC_URL = ['https://en.wikipedia.org/wiki/Cars_(film)', 'https://en.wikipedia.org/wiki/Cars_2', 'https://en.wikipedia.org/wiki/Cars_3']  # A comma-separated list of URLs to scrape
+WEB_CONTEXT = ['div', 'div', 'div']  # A comma-separated list of the tag or object to search for in each page above.
+WEB_ATTRIBUTES = [{'mw-content-ltr'}, {'mw-content-ltr'}, {'mw-content-ltr'}] # A list of dictionaries containing the attributes for each page.
 
-ODDS = 8  # How often do you want this to run? 1/8 times?
+ODDS = 4  # How often do you want this to run? 1/8 times?
 ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
 
-DEBUG = True  # Set this to False to start Tweeting live
-TWEET_ACCOUNT = ""  # The name of the account you're tweeting to.
+DEBUG = False  # Set this to False to start Tweeting live
+TWEET_ACCOUNT = 'benmountain8'  # The name of the account you're tweeting to.
 
 #Configuration for Twitter parser. TEST_SOURCE will be re-used as as the corpus location.
-TWITTER_ARCHIVE_NAME = "tweets.csv" #Name of your twitter archive
+TWITTER_ARCHIVE_NAME = 'tweets.csv' #Name of your twitter archive
 IGNORE_RETWEETS = True #If you want to remove retweets
