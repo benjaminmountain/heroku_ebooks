3297335c5fd16d37cac0a419af34babab1f54e0f
Ben Mountain <bemo1787@colorado.edu>
2021-04-30 02:41:07 -0600

    increase generated wordcount to 30

--- markov_3.py
+++ markov_3.py
@@ -1,9 +1,8 @@
 import numpy as np
 import twitter
-
 from local_settings import (MY_CONSUMER_KEY, MY_CONSUMER_SECRET, MY_ACCESS_TOKEN_KEY, MY_ACCESS_TOKEN_SECRET)
-# Adapted from https://towardsdatascience.com/simulating-text-with-markov-chains-in-python-1a27e6d13fc6
 
+# Adapted from https://towardsdatascience.com/simulating-text-with-markov-chains-in-python-1a27e6d13fc6
 
 def connect(type='twitter'): # connect to twitter API using keys from developer account
     if type == 'twitter':
@@ -43,7 +42,7 @@ def generate_script(carpus):
 
     chain = [first_word]
 
-    n_words = 15
+    n_words = 30
 
     for i in range(n_words):
         chain.append(np.random.choice(word_dict[chain[-1]]))
